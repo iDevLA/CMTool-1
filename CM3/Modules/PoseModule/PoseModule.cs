b50fe3b22408ec4a534ccb3b9177b057b72a970b
Yuuki <yuuki.walsh@outlook.com>
2020-03-28 18:39:06 +1100

    empty pose module

---
+++ CM3/Modules/PoseModule/PoseModule.cs
@@ -0,0 +1,24 @@
+﻿// Concept Matrix 3.
+// Licensed under the MIT license.
+
+namespace ConceptMatrix.PoseModule
+{
+	using System;
+	using System.Threading.Tasks;
+	using ConceptMatrix;
+	using ConceptMatrix.Modules;
+
+	public class PoseModule : IModule
+	{
+		public Task Initialize()
+		{
+			Log.Write("Hello!", "Pose Module");
+			return Task.CompletedTask;
+		}
+
+		public Task Shutdown()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
